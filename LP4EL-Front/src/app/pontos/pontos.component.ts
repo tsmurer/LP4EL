b849edb0cd028db1d8d105cf2ec9e15bce6bee4d
tiagoMurer <tiagostaurenghi@hotmail.com>
2020-10-07 13:52:19 -0300

    link e componente loja

---
+++ LP4EL-Front/src/app/pontos/pontos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pontos',
+  templateUrl: './pontos.component.html',
+  styleUrls: ['./pontos.component.scss']
+})
+export class PontosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
